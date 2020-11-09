43b5b32dc953e1cb243dfdca931aecde1aabe6b7
Matt Rosensweig <matthew.rosensweig@gmail.com>
2020-11-09 13:56:04 -0500

    Configure CORS middleware with client origin

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-const { NODE_ENV } = require('./config');
+const { NODE_ENV, CLIENT_ORIGIN } = require('./config');
 
 const app = express();
 
@@ -11,7 +11,11 @@ const morganOption = NODE_ENV === 'production' ? 'tiny' : 'dev';
 
 app.use(morgan(morganOption));
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN
+  })
+);
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
