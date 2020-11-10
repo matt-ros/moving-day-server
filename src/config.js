86e372a76a1dad884fe2aa52b3722b54d2a0a7d4
Matt Rosensweig <matthew.rosensweig@gmail.com>
2020-11-10 15:38:26 -0500

    Apply minor updates to maintain production database

--- src/config.js
+++ src/config.js
@@ -3,5 +3,7 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
-  DB_URL: process.env.DB_URL || 'postgresql://movingday@localhost/moving-day'
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://movingday@localhost/moving-day',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://movingday@localhost/moving-day-test'
+
 }
