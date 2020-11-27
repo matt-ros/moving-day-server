9ea21cbc8b098e64b4e7ca211ae2ad5a2f3ebd6e
Matt Rosensweig <matthew.rosensweig@gmail.com>
2020-11-27 16:31:54 -0500

    Update table seed data

--- seeds/seed.movingday_tables.sql
+++ seeds/seed.movingday_tables.sql
@@ -19,7 +19,7 @@ INSERT INTO movingday_users (
     'Demo User',
     '$2a$12$aIOKQoUYBOsZZxJ1Py2CmumTKiNNaIDc4R6rQ6/3Ja1hAPvarnzH6',
     '2020-12-31',
-    'These are some notes for the demo user.  Lots and lots of notes.  Look at all the notes!'
+    'These are some notes for the demo user.'
   ),
   (
     'demo2',
